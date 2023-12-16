e6c3ca377f012a51578aa35b9c8178b87d920ec9
ziaulh53 <ziaul.siu@gmail.com>
2023-12-17 05:12:13 +0600

    done

---
+++ dist/service-worker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'my-cache-v1';
+
+// Files to cache
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Add more files to cache as needed
+];
+
+// Event listener for the 'install' event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(cacheFiles);
+      })
+  );
+});
+
+// Event listener for the 'fetch' event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return the response from the cache
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to make a network request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then((response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to put it in the cache
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
+
+// Event listener for the 'activate' event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
